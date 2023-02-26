332c40529a141a046fd4575c8b11d064bb1177ce
SergioAJS <SergFanMadrid@gmail.com>
2023-02-27 01:35:01 +0500

    fix: swagger.ts

--- gf-server-api/swagger.ts
+++ gf-server-api/swagger.ts
@@ -7,7 +7,7 @@ config();
 export const GF_SERVER_API_HOST = process.env.GF_SERVER_API_HOST;
 export const GF_SERVER_API_PORT = process.env.GF_SERVER_API_PORT;
 
-swaggerDefinition.servers[0].url = `${GF_SERVER_API_HOST}:${GF_SERVER_API_PORT}`;
+swaggerDefinition.servers[0].url = "https://golden-fleece-server-api.onrender.com/";
 
 const swaggerJsDocOptions = {
   swaggerDefinition,
